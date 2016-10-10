a7d833cc41ddf3018584d0de27e163c6883767b5
owen <owen002@gmail.com>
2016-10-10 20:20:39 +0800

    百度统计代码

---
+++ javascripts/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?ff120f614cc6728276bc3e2c69f92692";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
