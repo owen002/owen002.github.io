d3ed8af7c2adf8f5adc6ba76d447557690d7c5f4
raul <owen002@gmail.com>
2016-11-01 11:31:42 +0800

    mdi

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,21 +1,30 @@
 (function () {
-    var pageTitles = [], pageContext = [], pageAddr = [], sorts = ['工具', '前端'], sortsIndex = {};
+    var pageTitles = [], pageContext = [], pageAddr = [], sorts = ['工具', '前端', '问题'], sortsIndex = {};
     sortsIndex = {
         0: [0],
-        1: [1, 2]
+        1: [1, 2, 4],
+        3: [3]
     };
-    //markdown语法
+    //markdown语法0
     pageTitles.push('markdown语法');
     pageContext.push('markdown的基本使用方法');
     pageAddr.push('https://owen002.github.io/pages/myblog/Markdown.html');
-    //css画三角形
+    //css画三角形1
     pageTitles.push('css画三角形');
     pageContext.push('使用纯css画一个三角形');
     pageAddr.push('https://owen002.github.io/pages/myblog/triangle-css.html');
-    //数组去重
+    //数组去重2
     pageTitles.push('数组去重');
     pageContext.push('js数组去重的几种方法');
     pageAddr.push('https://owen002.github.io/pages/myblog/arr_repeat.html');
+    //module_not_found_pro3
+    pageTitles.push('module_not_found问题');
+    pageContext.push('使用webpack构建react项目报错Module not found: Error: Cannot resolve module...');
+    pageAddr.push('https://owen002.github.io/pages/myblog/module_not_found_problem.html');
+    //ES6_React_constructor4
+    pageTitles.push('ES6_React_constructor');
+    pageContext.push('使用ES6编写React组件');
+    pageAddr.push('https://owen002.github.io/pages/myblog/ES6_React_constructor.html');
 
     var $sideBar = $('#sidebar');
     var page = {
